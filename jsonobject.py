3dd7c62b9627bb0879f6e9eeb9ba4ceddb8cd39e
dataopensystem <dataopensystem@outlook.com>
2020-01-18 11:46:50 -0300

    Updated

--- jsonobject.py
+++ jsonobject.py
@@ -15,7 +15,6 @@ class  JSONObject(object):
         value = value.replace("\"","'")
         _dict = eval(value)
         self.__dict__ = self.__convertto(_dict)
-
     def __convertto(self,dictvalue):
         _dict = {}
         for key , value in dictvalue.items():
@@ -29,6 +28,23 @@ class  JSONObject(object):
         else:
                 return value
 class JSON(object):
+    def __init__(self):
+        self.__colon = ':'
+        self.__comma = ','
+        self.__leftbracket = '['
+        self.__true = 'True'
+        self.__false = 'False'
+        self.__null = 'null'
+        self.__none = 'None'
+    def __iscolon(self,value):
+        return  value  ==  self.__colon
+    def __iscomma(self,value):
+        return value == self.__comma
+    def __isleftbracket(self,value):
+        return self.__leftbracket == value
+    def converttoobject(self,value):
+        """Convert to Object and return one."""
+        return JSONObject(self.__topython(value))
     def converttojson(self,obj):
         """Convert to JSON and return one."""
         _dict = { }
@@ -37,7 +53,16 @@ class JSON(object):
                  self.__setvalue(_dict,key,value)
         else:
             self.__exception(obj)
-        return str(_dict).replace("'","\"")
+        return self.__tojson(str(_dict))
+    def converttojson(self,obj):
+        """Convert to JSON and return one."""
+        _dict = { }
+        if issubclass(obj.__class__, JSONObject):
+            for key, value in obj.__dict__.items():
+                 self.__setvalue(_dict,key,value)
+        else:
+            self.__exception(obj)
+        return self.__tojson(str(_dict))
     def __getitem_convertfrom(self,obj):
         if type(obj) is dict:
             return obj
@@ -48,12 +73,18 @@ class JSON(object):
             return _dict
         elif type(obj) is list:
             return [self.__getitem_convertfrom(index) for index in obj]
+        elif type(obj) is tuple:
+            return [self.__getitem_convertfrom(index) for index in obj]
         elif type(obj) is int:
             return obj
         elif type(obj) is float:
             return obj
         elif type(obj) is str:
             return obj
+        elif type(obj) is bool:
+            return obj
+        elif obj is None:
+            return None
         else:
            self.__exception(obj)
     def __exception(self,obj):
@@ -69,6 +100,45 @@ class JSON(object):
             return key[1]
         else:
             return value
-    def converttoobject(self,value):
-        """Convert to Object and return one."""
-        return JSONObject(value)
+    def __gettopython(self,value,index,length):
+        if value[index+1:index+1+len(self.__true)] == self.__true.lower():
+            return value[:index+1] + value[index+1:index+1+len(self.__true)].capitalize() +  value[index+1+len(self.__true):]
+        elif value[index+1:index+1+len(self.__false)] == self.__false.lower():
+            return value[:index+1] + value[index+1:index+1+len(self.__false)].capitalize() +  value[index+1+len(self.__false):]
+        elif value[index+1:index+1+len(self.__null)] == self.__null.lower():
+            return value[:index+1] + self.__none.capitalize() +  value[index+1+len(self.__none):]
+        return value
+    def __topython(self, value):
+        value = value.replace(' ','')
+        index = 0
+        length = len(value)
+        while(index < length):
+          if self.__iscolon(value[index]):
+            value = self.__gettopython(value,index,length)
+          elif self.__isleftbracket(value[index]):
+            value = self.__gettopython(value,index,length)
+          elif self.__iscomma(value[index]):
+            value = self.__gettopython(value,index,length)
+          index+=1
+        return value.replace('\"','\'')
+    def __tojson(self, value):
+        value = value.replace(' ','')
+        index = 0
+        length = len(value)
+        while(index < length):
+            if self.__iscolon(value[index]):
+                value = self.__gettojson(value,index,length)
+            elif self.__isleftbracket(value[index]):
+                value = self.__gettojson(value,index,length)
+            elif self.__iscomma(value[index]):
+                value = self.__gettojson(value,index,length)
+            index+=1
+        return value.replace('\'','\"')
+    def __gettojson(self,value,index,length):
+        if value[index+1:index+1+len(self.__true)] == self.__true.capitalize():
+            return value[:index+1] + value[index+1:index+1+len(self.__true)].lower() +  value[index+1+len(self.__true):]
+        elif value[index+1:index+1+len(self.__false)] == self.__false.capitalize():
+            return  value[:index+1] + value[index+1:index+1+len(self.__false)].lower() +  value[index+1+len(self.__false):]
+        elif value[index+1:index+1+len(self.__none)] == self.__none.capitalize():
+            return  value[:index+1] + self.__null.lower() +  value[index+1+len(self.__none):]
+        return value
