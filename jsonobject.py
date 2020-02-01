2442798d840cb743a6702643003b9fb608ace169
dataopensystem <dataopensystem@outlook.com>
2020-02-01 09:01:10 -0300

    Add method toseparate

--- jsonobject.py
+++ jsonobject.py
@@ -27,7 +27,11 @@ class JSON(object):
     def __init__(self):
         self.__colon = ':'
         self.__comma = ','
-        self.__leftbracket = '['
+        self.__space = ' '
+        self.__leftbrace = '{'
+        self.__rightbrace = '}'
+        self.__leftbracket ='['
+        self.__rightbracket = ']'
         self.__true = 'True'
         self.__false = 'False'
         self.__null = 'null'
@@ -38,6 +42,29 @@ class JSON(object):
         return value == self.__comma
     def __isleftbracket(self,value):
         return self.__leftbracket == value
+    def __isrightbracket(self,value):
+        return  value  ==  self.__rightbracket
+    def __isrightbrace(self,value):
+        return  value  ==  self.__rightbrace
+    def __isleftbrace(self,value):
+        return  value  ==  self.__leftbrace
+    def __insert(self,value,index,key):
+        return value[:index] + key + value[index:]
+    def __toseparate(self,json):
+        get = json
+        length = len(get)
+        index = 0
+        while index < length:
+            if self.__isrightbrace(get[index]) or self.__isrightbracket(get[index]) or self.__iscolon(get[index]):
+                get = self.__insert(get,index,self.__space)
+                length = len(get)
+                index += 1
+            if  self.__isleftbrace(get[index]) or self.__isleftbracket(get[index]) or self.__iscomma(get[index]):
+                get = self.__insert(get,index+1,self.__space)
+                length = len(get)
+                index += 1
+            index +=1
+        return get
     def converttoobject(self,value):
         """Convert to Object and return one."""
         return JSONObject(self.__topython(value))
@@ -86,7 +113,7 @@ class JSON(object):
     def __topython(self, value):
         if type(value) is bytes:
             value = value.decode('utf-8')
-        value = value.replace(' ','')
+        value = self.__toseparate(value)
         index = 0
         length = len(value)
         while(index < length):
@@ -101,7 +128,7 @@ class JSON(object):
     def __tojson(self, value):
         if type(value) is bytes:
             value = value.decode('utf-8')
-        value = value.replace(' ','')
+        value = self.__toseparate(value)
         index = 0
         length = len(value)
         while(index < length):
